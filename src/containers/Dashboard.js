32e2e43868eaef5c6b7fb158f8359ab0b40f33a3
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-07-06 15:44:18 +0200

    fixed issue for admin dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -75,6 +75,7 @@ export default class {
     $('#arrow-icon1').click((e) => this.handleShowTickets(e, bills, 1))
     $('#arrow-icon2').click((e) => this.handleShowTickets(e, bills, 2))
     $('#arrow-icon3').click((e) => this.handleShowTickets(e, bills, 3))
+    this.getBillsAllUsers()
     new Logout({ localStorage, onNavigate })
   }
 
@@ -146,6 +147,7 @@ export default class {
     }
 
     bills.forEach(bill => {
+      $(`#open-bill${bill.id}`).off("click")
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
