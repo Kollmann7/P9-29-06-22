aeb110bdf76205ad48be11234679f460227c71e1
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-07-14 14:41:05 +0200

    test if bill is creat and add before each

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -6,6 +6,8 @@ import { screen, fireEvent } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import {localStorageMock} from "../__mocks__/localStorage.js"
+import { ROUTES } from '../constants/routes'
+
 
 
 
@@ -31,11 +33,14 @@ describe("Given I am connected as an employee", () => {
   })
 })
 describe("When I'm on NewBill Page", () => {
-  describe('And I upload an image file', () => {
-    test('Then the file extension is correct', () => {
+  let newBill 
+    beforeEach( () => {
       document.body.innerHTML = NewBillUI()
-      const newBill = new NewBill({document, onNavigate, store: null, localStorage: window.localStorage})
+      newBill = new NewBill({document, onNavigate, store: null, localStorage: window.localStorage})
 
+    })
+  describe('And I upload an image file', () => { 
+    test('Then the file extension is correct', () => {
       const handleChangeFile = jest.fn(() => newBill.handleChangeFile)
       const inputFile = screen.queryByTestId('file')
 
@@ -50,10 +55,7 @@ describe("When I'm on NewBill Page", () => {
       const error = screen.queryByTestId('errorMessage')
       expect(error).toBeFalsy
     })
-    test('Then the file extension is incorrect', () => {
-      document.body.innerHTML = NewBillUI()
-      const newBill = new NewBill({document, onNavigate, store: null, localStorage: window.localStorage})
-
+    test('Then the file extension is incorrect', () => {    
       const handleChangeFile = jest.fn(() => newBill.handleChangeFile)
       const inputFile = screen.queryByTestId('file')
 
@@ -73,5 +75,40 @@ describe("When I'm on NewBill Page", () => {
       expect(error).toBeTruthy
     })
   })
+  describe('When I submit a valid bill', () => {
+    test('Then a bill is created', () =>{
+      const submit = screen.queryByTestId('form-new-bill')
+      const billTest = {
+        name: 'Test-Bill',
+        pct: 10,
+        vat: 10,
+        date: '2022-07-13',
+        amount: 180,
+        commentary: 'test de test',
+        type: 'Bar',
+        fileName: 'testBill',
+        fileUrl: 'testBill.jpg',
+      };
+  
+      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e))
+  
+      newBill.createBill = (newBill) => newBill
+  
+      document.querySelector(`input[data-testid="expense-name"]`).value = billTest.name
+      document.querySelector(`input[data-testid="pct"]`).value = billTest.pct
+      document.querySelector(`input[data-testid="vat"]`).value = billTest.vat
+      document.querySelector(`input[data-testid="datepicker"]`).value = billTest.date
+      document.querySelector(`input[data-testid="amount"]`).value = billTest.amount
+      document.querySelector(`textarea[data-testid="commentary"]`).value = billTest.commentary
+      document.querySelector(`select[data-testid="expense-type"]`).value = billTest.type  
+      newBill.fileUrl = billTest.fileUrl
+      newBill.fileName = billTest.fileName
+  
+      submit.addEventListener('click', handleSubmit)
+  
+      fireEvent.click(submit)
+  
+      expect(handleSubmit).toHaveBeenCalled()
+    })
+  })
 })
-
