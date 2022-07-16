bce93d3cb40a91adbfda9aae9082f20eb26e1f21
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-07-16 16:11:59 +0200

    Delete space

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -75,7 +75,6 @@ export default class {
     $('#arrow-icon1').click((e) => this.handleShowTickets(e, bills, 1))
     $('#arrow-icon2').click((e) => this.handleShowTickets(e, bills, 2))
     $('#arrow-icon3').click((e) => this.handleShowTickets(e, bills, 3))
-    this.getBillsAllUsers()
     new Logout({ localStorage, onNavigate })
   }
 
