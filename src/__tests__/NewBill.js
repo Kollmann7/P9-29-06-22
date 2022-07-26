8feabae11daac160a676967462aba3015de2bc7e
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-07-26 14:41:56 +0200

    Add int and 500 and 404 error

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -7,6 +7,11 @@ import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import {localStorageMock} from "../__mocks__/localStorage.js"
 import { ROUTES } from '../constants/routes'
+import mockStore from '../__mocks__/store'
+import { bills } from '../fixtures/bills.js'
+import BillsUI from '../views/BillsUI.js'
+
+
 
 const onNavigate = (pathname) => {
   document.body.innerHTML = ROUTES({ pathname })
@@ -106,6 +111,111 @@ describe("When I'm on NewBill Page", () => {
       fireEvent.click(submit)
   
       expect(handleSubmit).toHaveBeenCalled()
+    })
+  })
+})
+
+describe('Given I am connected as an employee', () => {
+  describe('When I am on NewBill page and I upload a file with jpg, jpeg or png extension', () => {
+    test('Then a new bill should be stored in the API', async () => {
+      document.body.innerHTML = NewBillUI()
+
+      const spyCreate = jest.spyOn(mockStore.bills(), 'create')
+      const bill = await spyCreate()
+
+      expect(spyCreate).toHaveBeenCalled()
+
+      expect(bill.key).toBe('1234')
+      expect(bill.fileUrl).toBe('https://localhost:3456/images/test.jpg')
+    })
+  })
+  describe('When an error occurs on API', () => {
+    beforeEach(() => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem(
+        'user',
+        JSON.stringify({
+          type: 'Employee',
+          email: 'a@a',
+        })
+      )
+
+      document.body.innerHTML = NewBillUI()
+    })
+
+    test('Then new bill are fetch from API but fails with 404 message error', async () => {
+      const spyedMockStore = jest.spyOn(mockStore, 'bills')
+
+      spyedMockStore.mockImplementationOnce(() => {
+        return {
+          create: jest.fn().mockRejectedValue(new Error('Erreur 404')),
+        }
+      })
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname, data: bills })
+      }
+      const newBill = new NewBill({ document,onNavigate,store: mockStore,bills: bills,localStorage: window.localStorage,})
+      const inputFile = screen.queryByTestId('file')
+
+      fireEvent.change(inputFile, {
+        target: {
+          files: [
+            new File(['test'], 'test.txt', {
+              type: 'text/plain',
+            }),
+          ],
+        },
+      })
+
+      
+      await spyedMockStore()
+
+      expect(spyedMockStore).toHaveBeenCalled()
+
+      expect(newBill.billId).toBeNull()
+      expect(newBill.fileUrl).toBeNull()
+      spyedMockStore.mockReset()
+      spyedMockStore.mockRestore()
+    })
+    test('Then new bill are fetch from API but fails with 500 message error', async () => {
+      const spyedMockStore = jest.spyOn(mockStore, 'bills')
+
+      spyedMockStore.mockImplementationOnce(() => {
+        return {
+          create: jest.fn().mockRejectedValue(new Error('Erreur 500')),
+        }
+      })
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname, data: bills })
+      }
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        bills: bills,
+        localStorage: window.localStorage,
+      })
+
+      const inputFile = screen.queryByTestId('file')
+
+      fireEvent.change(inputFile, {
+        target: {
+          files: [
+            new File(['test'], 'test.txt', {
+              type: 'text/plain',
+            }),
+          ],
+        },
+      })
+
+      await spyedMockStore()
+
+      expect(spyedMockStore).toHaveBeenCalled()
+
+      expect(newBill.billId).toBeNull()
+      expect(newBill.fileUrl).toBeNull()
     })
   })
 })
